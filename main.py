7793543c5082110cc6a8669ebfd06e6f9f444007
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-04-07 00:15:14 +0300

    переработка зависимостей, файл с кнопками, меню прошивок

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from menu import start_main_menu
 from flash_menu import start_flash_menu
+import time
 
-def main():
-    start_main_menu(open_flash_menu=start_flash_menu)
+# Глобальная переменная текущего меню
+current_menu = None
 
-if __name__ == "__main__":
-    main()
+# Функции переключения меню
+def go_to_main_menu():
+    global current_menu
+    current_menu = lambda: start_main_menu(go_to_flash_menu)
+
+def go_to_flash_menu():
+    global current_menu
+    current_menu = lambda: start_flash_menu(go_to_main_menu)
+
+# Запускаем с главного меню
+go_to_main_menu()
+
+# Основной цикл
+while True:
+    current_menu()  # вызываем активное меню
+    time.sleep(0.1)  # не грузим проц
