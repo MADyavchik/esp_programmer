bc1a3296c729519ae07bb9ae7a146ec0f4dfc674
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-04-06 16:34:07 +0300

    Перзапуск после обновления

--- git_update.py
+++ git_update.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+import sys
 
 # Функция для обновления репозитория
 def update_repo():
@@ -9,7 +11,12 @@ def update_repo():
 
         if result.returncode == 0:
             print("[GIT] Репозиторий успешно обновлен!")
+
+            # Перезапуск программы только после успешного обновления
+            print("[GIT] Перезапуск программы...")
+            os.execv(sys.executable, ['python3'] + sys.argv)  # Перезапуск текущего скрипта (main.py)
         else:
             print(f"[GIT] Ошибка при обновлении: {result.stderr}")
+
     except Exception as e:
         print(f"[GIT] Ошибка: {e}")
