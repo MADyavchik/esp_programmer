df3c35880c0e9c3e16bb2b54974e51917ab05c25
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-04-10 00:09:30 +0300

    автоперезапуск при ошибке

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 # main.py
 import time
 import traceback
-import os
 from menu_navigation import run_menu_loop
-
-LOG_FILE = "/home/pauro/esp_programmer/error.log"  # можно изменить путь
+from oled_ui import clear
 
 def log_error(e):
-    with open(LOG_FILE, "a") as f:
-        f.write(f"\n--- ERROR at {time.ctime()} ---\n")
+    with open("error.log", "a") as f:
+        f.write(f"{time.ctime()}: {repr(e)}\n")
         traceback.print_exc(file=f)
 
 def main():
     while True:
         try:
             run_menu_loop()
+        except KeyboardInterrupt:
+            print("⏹ Выход по Ctrl+C")
+            clear()
+            break
         except Exception as e:
             print("💥 Программа упала с ошибкой, перезапуск через 2 секунды")
             traceback.print_exc()
             log_error(e)
-            time.sleep(2)  # немного подождём перед перезапуском
+            time.sleep(2)
 
 if __name__ == "__main__":
     main()
