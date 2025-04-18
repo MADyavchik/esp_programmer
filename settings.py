211c373be6470b67fa6b78f61596695c29d9f807
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-04-19 02:03:31 +0300

    добавление меню настроек + пункт принтера + ассинхронка снова

--- settings.py
+++ settings.py
@@ -86,7 +86,7 @@ async def start_settings_menu():
     def back():
         selected_result[0] = "main"
 
-    setup_buttons(up, down, back, lambda: asyncio.create_task(select()))
+    setup_buttons(up, down, back, select)
 
     draw()
     while selected_result[0] is None:
