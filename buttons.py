9f97de840742dd777cc2ae439414f3eafab6f51e
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-04-19 02:18:15 +0300

    добавление меню настроек + пункт принтера + ассинхронка снова

--- buttons.py
+++ buttons.py
@@ -6,17 +6,17 @@ btn_down = Button(13, bounce_time=0.1)
 btn_back = Button(6, hold_time=3, bounce_time=0.1)
 btn_select = Button(19, bounce_time=0.1)
 
-def safe_async(coro):
+def safe_async(coro_func):
     try:
         loop = asyncio.get_running_loop()
-        loop.call_soon_threadsafe(lambda: asyncio.create_task(coro))
+        loop.call_soon_threadsafe(lambda: asyncio.create_task(coro_func()))
     except RuntimeError:
         print("⚠️ Нет активного event loop для safe_async")
 
 def setup_buttons(up, down, back, select, back_hold_action=None, up_hold_action=None):
     def wrap(func):
         if asyncio.iscoroutinefunction(func):
-            return lambda: safe_async(func())
+            return lambda: safe_async(func)  # <-- БЕЗ ВЫЗОВА ЗДЕСЬ
         return func
 
     btn_up.when_pressed = wrap(up)
