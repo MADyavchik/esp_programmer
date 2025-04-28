229492eb6758bae6891a0a6d50fc2111b3474d99
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-04-28 17:28:59 +0300

    универсальная функция для отрисовки меню

--- oled_ui.py
+++ oled_ui.py
@@ -21,13 +21,12 @@ status_data = {"battery": "--%", "wifi": "--", "charging": False}
 def display_on_all(image):
     if st_device:
         try:
-            print("Запускаем ресайз изображения.")
+            clear()
+            print("хуй")
             image_st = image.resize((240, 240), Image.Resampling.LANCZOS)
-            print("Ресайз завершен.")
 
-            print("Попытка отобразить изображение.")
             st_device.display_image(image_st)
-            print("Изображение успешно отображено.")
+
         except Exception as e:
             print("ST7789 display error:", e)
 
