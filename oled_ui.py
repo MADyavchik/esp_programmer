f9e1084dabb53bfec8b443db3190ada181e40f3d
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-06-16 14:27:02 +0300

    logir testa+status

--- oled_ui.py
+++ oled_ui.py
@@ -111,32 +111,48 @@ def draw_log_table(data, status = None):
     image = Image.new("RGB", (240, 240), "black")
     draw = ImageDraw.Draw(image)
 
-    marker = "white"
+    if status == "OK":
+        draw.text((0, 0), f"BATT: {data['Battery']}", font=font, fill="green")
+    elif status == "FAIL":
+        draw.text((0, 0), f"BATT: {data['Battery']}", font=font, fill="red")
+    else:
+        draw.text((0, 0), f"BATT: {data['Battery']}", font=font, fill="white")
 
     if status == "OK":
-        marker = "green"
+        draw.text((0, 0), f"W: {data['Weight']}", font=font, fill="green")
     elif status == "FAIL":
-        marker = "red"
+        draw.text((0, 0), f"W: {data['Weight']}", font=font, fill="red")
     else:
-        marker = "white"
+        draw.text((0, 0), f"W: {data['Weight']}", font=font, fill="white")
 
-    draw.text((0, 0), f"BATT: {data['Battery']}", font=font, fill=marker)
-    #draw.text((0, 0), f"BATT: {data.get('Battery', '—')}", font=font, fill="white")
+    if status == "OK":
+        draw.text((0, 0), f"TEMP: {data['Temp']}", font=font, fill="green")
+    elif status == "FAIL":
+        draw.text((0, 0), f"TEMP: {data['Temp']}", font=font, fill="red")
+    else:
+        draw.text((0, 0), f"TEMP: {data['Temp']}", font=font, fill="white")
 
-    draw.text((0, 40), f"W: {data['Weight']}", font=font, fill=marker)
-    #draw.text((0, 40), f"WEIGHT: {data.get('Weight', '—')}", font=font, fill="white")
+    if status == "OK":
+        draw.text((0, 0), f"TOF: {data['TOF']}", font=font, fill="green")
+    elif status == "FAIL":
+        draw.text((0, 0), f"TOF: {data['TOF']}", font=font, fill="red")
+    else:
+        draw.text((0, 0), f"TOF: {data['TOF']}", font=font, fill="white")
 
-    draw.text((0, 80), f"TEMP: {data['Temp']}", font=font, fill="white")
-    #draw.text((0, 80), f"TEMP: {data.get('Temp', '—')}", font=font, fill="white")
+    if status == "OK":
+        draw.text((0, 0), f"CPU t: {data['CPU Temp']}", font=font, fill="green")
+    elif status == "FAIL":
+        draw.text((0, 0), f"CPU t: {data['CPU Temp']}", font=font, fill="red")
+    else:
+        draw.text((0, 0), f"CPU t: {data['CPU Temp']}", font=font, fill="white")
 
-    draw.text((0, 120), f"TOF: {data['TOF']}", font=font, fill="white")
-    #draw.text((0, 120), f"TOF: {data.get('TOF', '—')}", font=font, fill="white")
+    if status == "OK":
+        draw.text((0, 0), f"RSSI: {data['DOM.Online']}", font=font, fill="green")
+    elif status == "FAIL":
+        draw.text((0, 0), f"RSSI: {data['DOM.Online']}", font=font, fill="red")
+    else:
+        draw.text((0, 0), f"RSSI: {data['DOM.Online']}", font=font, fill="white")
 
-    draw.text((0, 160), f"CPU t: {data['CPU Temp']}", font=font, fill=marker)
-    #draw.text((0, 160), f"CPU t: {data.get('CPU Temp', '—')}", font=font, fill="white")
-
-    draw.text((0, 200), f"RSSI: {data['DOM.Online']}", font=font, fill="white")
-    #draw.text((0, 200), f"RSSI: {data.get('DOM.Online', '—')}", font=font, fill="white")
     display_on_all(image)
 
 def update_status_data(battery, wifi, charging=False):
