9571c6fcedde813ff681c2ce5aa23f05314eeb84
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-06-24 02:59:24 +0300

    inactivity_watcher_2

--- screens/shotdown_screen.py
+++ screens/shotdown_screen.py
@@ -4,7 +4,7 @@ import asyncio
 import os
 import time
 import state
-from oled_ui import show_message
+from oled_ui import show_message, clear
 
 from st7789_pi import ST7789
 st_device = ST7789(width=240, height=240, dc=23, reset=24, bl=12)
@@ -29,6 +29,7 @@ async def run_shotdown_halt():
         await asyncio.sleep(1)
 
     print("⏹️ Завершение работы устройства...")
+    clear()
     st_device.set_backlight(False)
     #state.shutdown_pending = False
     await asyncio.sleep(0.5)
