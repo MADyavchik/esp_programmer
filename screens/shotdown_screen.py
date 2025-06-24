ca97de26f7ca5813553d6336d5e01d7b9f147b7d
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-06-24 03:05:05 +0300

    inactivity_watcher_2

--- screens/shotdown_screen.py
+++ screens/shotdown_screen.py
@@ -4,10 +4,8 @@ import asyncio
 import os
 import time
 import state
-from oled_ui import show_message, clear
+from oled_ui import show_message, clear, st_device
 
-from st7789_pi import ST7789
-st_device = ST7789(width=240, height=240, dc=23, reset=24, bl=12)
 
 async def run_shotdown_halt():
     print("⚠️ Без активности. Запуск таймера выключения...")
